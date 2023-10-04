36dd45ebbd6732098f545e8c37a2a95f1c939a24
AndresIdk <jnavarrou@unicartagena.com>
2023-10-04 09:58:22 -0500

    Two ports

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 // express server
 const express = require("express");
-const app = express();
+const app1 = express(); // Nginx 1
+const app2 = express(); // Nginx 2
 
 // Configura la carpeta estática para servir archivos CSS y otros recursos
-app.use(express.static(__dirname));
+app1.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: __dirname });
+// Ruta servidor 1
+app1.get('/', (req, res) => {
+  res.sendFile('index1.html', {root: __dirname });
 });
 
-app.listen(3008, () => {
+// Ruta servidor 2
+app2.get('/', (req, res) => {
+  res.sendFile('index2.html', {root: __dirname });
+});
+
+app1.listen(3008, () => {
     console.log("server started on port 3008");
 });
 
-
+app2.listen(3100, () => {
+  console.log("server started on port 3100");
+});
